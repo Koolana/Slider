6c3f708dbab48b7e3728267be0667f20ef8ac3d6
Koolana <kolya_andreychik@mail.ru>
2019-04-06 01:41:20 +0300

    start

--- js/simpleScript.js
+++ js/simpleScript.js
@@ -6,7 +6,7 @@ date: 23.03.19
 $(document).ready(function(){
 	var slider1 = {
 		withGrab: true,
-		time: 1000,
+		time: false,
 		animationTime: 500, //в ms
 
 		numActSlide: 3,
@@ -21,7 +21,15 @@ $(document).ready(function(){
 		withGrab: true,
 		time: false,
 		timeDelay: 500, //время до разблокировки действия
+		animationTime: 1000 //в ms
+	}
+
+	var slider3 = {
+		time: 2000,
+		timeDelay: 500, //время до разблокировки действия
 		animationTime: 1000, //в ms
+
+		accessCopies: true //сделать разрешение на копии
 	}
 
 	LYS.addSliderСarousel($('.slider#i1'), slider1);
@@ -30,6 +38,15 @@ $(document).ready(function(){
 	LYS.addSliderType1($('.slider#i2'), slider2);
 	LYS.addLeftRightButtons($('.slider#i2'), $('.buttonLeft#i2'), $('.buttonRight#i2'));
 	LYS.addAutoRowSwitchButtons($('.slider#i2'), $('.autoRowButtons#i2'));
+
+	/*-----test-----*/
+	LYS.addSliderСarousel($('.slider#i3'), slider1);
+	LYS.addLeftRightButtons($('.slider#i3'), $('.buttonLeft#i3'), $('.buttonRight#i3'));
+
+	LYS.addSliderType1($('.slider#i4'), slider3);
+	LYS.addSliderType1($('.slider#i5'), slider3);
+	LYS.addSliderType1($('.slider#i6'), slider3);
+	LYS.addSliderType1($('.slider#i7'), slider3);
 
 	LYS.addMouseControl();
 	LYS.addWindowSizeListener();
